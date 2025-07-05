17ce5ca9da8d14b1232fe8ef95f6d636766592b8
Stussyz <mfadhol07@gmail.com>
2025-07-05 19:53:55 +0700

    decision maker

---
+++ decisionMake1.js
@@ -0,0 +1,9 @@
+// A simple decision maker
+console.log('for example'); // first console
+
+// for example:
+if (1+1 === 2) {
+    console.log('true'); // this log will show only if the answer is true
+    } else { // for simplicity, i use else statement, not fully if if statement
+    console.log('false'); // if not, this log will showed up
+}
---
+++ decisionMake2.js
@@ -0,0 +1,10 @@
+//  if if decision maker with property Math.random
+let number = Math.random();
+console.log(number);
+
+if (number > 0.5) {
+    console.log ('this number is higher than 0.5');
+}
+if (number < 0.5) {
+    console.log ('this number is lower than 0.5');
+}
