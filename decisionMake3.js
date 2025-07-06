fa7f842526a7f9f1f2fabbe37d53d6b0963394b6
Stussyz <mfadhol07@gmail.com>
2025-07-06 18:57:02 +0700

    else - if decision statement with simple operator

---
+++ decisionMake3.js
@@ -0,0 +1,14 @@
+// else - if decision statement with simple operator 
+let nilai = 80;
+
+if (nilai <= 50) {
+    console.log ("nilai E");
+} else if (nilai <= 60) {
+    console.log ("nilai D");
+} else if (nilai <= 70) {
+    console.log ("nilai C");
+} else if (nilai <= 80) {
+    console.log ("nilai B");
+} else if (nilai < 100) {
+    console.log ("nilai A");
+}
