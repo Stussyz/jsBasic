3f5a58bf716185bf01b8541b79f3b96f5650b2be
Mohammad Fadhol <mfadhol07@gmail.com>
2025-07-09 17:03:05 +0700

    add OR decision

---
+++ decisionORLogic.js
@@ -0,0 +1,7 @@
+let position = prompt('input your position');
+
+if (position === 'manager' || position === 'spv') {
+    console.log ("Welcome to company, sir");
+} else {
+    console.log ("Access rejected");
+}
