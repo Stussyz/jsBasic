168ae8bf4600915636106361a891c0042bd9776f
Mohammad Fadhol <mfadhol07@gmail.com>
2025-07-09 17:02:17 +0700

    add AND decision with more example

--- decisionAndLogic.js
+++ decisionAndLogic.js
@@ -1,8 +1,18 @@
 // Reduce a lot of line code above with logic operator "And" (&&):
-const password = prompt('Please input 8-character password');
+// const password = prompt('Please input 8-character password');
 
-if (password.length >= 8 && password.indexOf(' ') === -1) {
-    console.log ('password valid');
+// if (password.length >= 8 && password.indexOf(' ') === -1) {
+//     console.log ('password valid');
+// } else {
+//     console.log ('password tidak sah')  
+// }
+
+// example 2 AND Operator:
+let age = prompt ('input your age');
+let gender = prompt ('input your gender');
+
+if (age === '24' && gender === 'male'){
+    console.log("memenuhi syarat");
 } else {
-    console.log ('password tidak sah')  
+    console.log("tidak memenuhi syarat");
 }
